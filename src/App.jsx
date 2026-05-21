ee811be565c60ee15058abb90704ce2732cabe14
yorleX23 <bogdandzikovskiy@gmail.com>
2026-05-21 18:22:17 +0300

    Підключаємо useContext до додатка

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+// src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { BookingProvider } from './context/BookingContext';
 import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import Booking from './pages/Booking';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/booking/:trainId" element={<Booking />} />
-      </Routes>
-      <ToastContainer position="bottom-right" />
-    </BrowserRouter>
+    <BookingProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/booking/:trainId" element={<Booking />} />
+        </Routes>
+        <ToastContainer position="bottom-right" />
+      </BrowserRouter>
+    </BookingProvider>
   );
 }
