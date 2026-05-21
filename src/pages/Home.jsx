8dc87a377d81b7fc387b866c5bdb55ab884a1504
yorleX23 <bogdandzikovskiy@gmail.com>
2026-05-21 18:23:05 +0300

    Переводимо сторінки на використання Context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
-import { trains } from '../data/trains';
+// src/pages/Home.jsx
+import { useState, useContext } from 'react';
+import { BookingContext } from '../context/BookingContext';
 import TrainList from '../components/TrainList';
 
 export default function Home() {
+  const { trains, loading } = useContext(BookingContext);
   const [search, setSearch] = useState('');
 
   const filteredTrains = trains.filter(train => 
@@ -10,13 +12,17 @@ export default function Home() {
     train.number.toLowerCase().includes(search.toLowerCase())
   );
 
+  if (loading) {
+    return <div className="container"><h3>Завантаження рейсів з mock-API...</h3></div>;
+  }
+
   return (
     <div className="container">
-      <h2>Розклад потягів 🚆</h2>
+      <h2>Розклад потягів Укрзалізниці 🚆</h2>
       <input 
         type="text" 
         className="search-input"
-        placeholder="Пошук за маршрутом (наприклад: Київ) або номером..." 
+        placeholder="Пошук за маршрутом або номером..." 
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
